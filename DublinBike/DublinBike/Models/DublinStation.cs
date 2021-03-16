faa2a44bd8a96c9856ed03a11e56adfea9d236ec
Guillaume Boulben <red56260@gmail.com>
2021-03-16 17:03:16 +0000

    voici

---
+++ DublinBike/DublinBike/Models/DublinStation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DublinBike.Models
+{
+    public class DublinStation
+    {
+        public string Name { get; set; }
+    }
+}

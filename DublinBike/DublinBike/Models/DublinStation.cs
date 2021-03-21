44f73a5fd6bf8e9d8ab92ff56fe638dc223fd7de
Lucas Leplanois <lucas.leplanois@epita.fr>
2021-03-21 17:48:50 +0100

    lucas's part
    Signed-off-by: Lucas Leplanois <lucas.leplanois@epita.fr>

--- DublinBike/DublinBike/Models/DublinStation.cs
+++ DublinBike/DublinBike/Models/DublinStation.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace DublinBike.Models
 {
     public class DublinStation
     {
         public string Name { get; set; }
+
+        public string Adress { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public int Number { get; set; }
+
     }
 }
---
+++ DublinBike/DublinBike/Models/DublinStationGenre.cs
@@ -0,0 +1,20 @@
+﻿using DublinBike.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DublinBike.Models
+{
+    public class DublinStationGenre
+    {
+        public List<DublinStation> DublinStations { get; set; }
+        public SelectList Names { get; set; }
+
+        public string StationName { get; set; }
+
+        public string SearchBar { get; set; }
+
+    }
+}
